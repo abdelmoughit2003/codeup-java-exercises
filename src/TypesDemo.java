261268b36fa4b5a03ca9217141b14fd7d30bc404
abdou <laajali.abdelmoughit@gmail.com>
2017-01-03 22:19:40 -0600

    TypesDemo java

---
+++ src/TypesDemo.java
@@ -0,0 +1,46 @@
+/**
+ * Created by sim on 1/3/17.
+ */
+public class TypesDemo {
+
+// create variables of every primitive type and print them to the console
+// try to assign a character to a variable that has been declared as an int
+// try to define a variable with the name class
+// initialize a variable, but do not assign it a value, then try and print it to the console
+    public static void main(String[] args) {
+        // Declaring a byte variable
+        byte ByteNumber= 127;
+        System.out.println(ByteNumber);
+
+        short ShortNumber= 2456;
+        System.out.println(ShortNumber);
+
+        int IntNumber= 1234557;
+        System.out.println(IntNumber);
+
+        long LongNumber=123456789111111L;
+        System.out.println(LongNumber);
+
+        float FloatNumber= 1.1111F;
+        System.out.println(FloatNumber);
+
+        double DoubleNumber=1.111;
+        System.out.println(DoubleNumber);
+
+        char Character ='C';
+        System.out.println(Character);
+
+        boolean IsTuesday = true;
+        System.out.println(IsTuesday);
+
+        int CharacterToInteger= Character;
+        System.out.println(CharacterToInteger);
+
+        int TypesDemo = 123;
+        System.out.println(TypesDemo);
+
+
+
+    }
+
+}
