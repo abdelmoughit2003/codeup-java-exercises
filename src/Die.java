2328c5128c12b9bee8a4678b7c3ce824a61a20ea
abdou <laajali.abdelmoughit@gmail.com>
2017-01-09 12:35:02 -0600

    java file

--- src/Die.java
+++ src/Die.java
@@ -6,8 +6,7 @@ import java.util.Scanner;
 public class Die {
     public static void main(String[] args) {
 
-        int f=1;
-        int g=2;
+
         int numberOfdSidesPerDie;
         String userinput;
         //Using do while to ask user if he wants to continue
@@ -28,7 +27,6 @@ public class Die {
 
             System.out.println("you rolled "+ dieOne.CurrentValue + " and " + dieTwo.CurrentValue);
             System.out.println("Your total is "+ (dieOne.CurrentValue + dieTwo.CurrentValue));
-            System.out.println(f+g);
 
             //Asking to continue
             System.out.println("Would you continue ? ");
